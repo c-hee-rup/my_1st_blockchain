8ab9e4e062e413b50d80c0882ad18f54f53bf823
c-hee-rup <38677485+c-hee-rup@users.noreply.github.com>
2018-04-26 21:36:54 +0900

    add if >,<

--- class.rb
+++ class.rb
@@ -9,6 +9,14 @@ class Blockchain
 	def mining
 		@number_of_blocks = @number_of_blocks + 1
 		history = []
+		if @number_of_blocks < 100
+		begin
+			nonce = rand(1000000)
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)
+			history << nonce
+		end while hashed[0..1] != '00'
+		history
+		if @number_of_blocks > 200
 		begin
 			nonce = rand(1000000)
 			hashed = Digest::SHA256.hexdigest(nonce.to_s)
