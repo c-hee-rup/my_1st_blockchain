9ebad5d04ad48744615fbc66199ea42e68470f85
c-hee-rup <38677485+c-hee-rup@users.noreply.github.com>
2018-04-26 22:52:43 +0900

    완성

--- class.rb
+++ class.rb
@@ -8,12 +8,15 @@ class Blockchain
 
 	def mining
 		begin
-			nonce = rand(100)
-		end while nonce != 0
+			nonce = rand(1000000)
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)
+		end while hashed[0..1] != '00'
 
 		block = {
 			'index' => @chain.length + 1,
-			'time' => Time.now.to_i
+			'time' => Time.now.to_i,
+			'nonce' => nonce,
+			'previous_block' => Digest::SHA256.hexdigest(last_block.to_s)
 		}
 		@chain << block
 	end
@@ -22,4 +25,8 @@ class Blockchain
 		@chain
 	end
 	
+	def last_block
+		@chain[-1]
+	end
+  
 end
--- web.rb
+++ web.rb
@@ -5,8 +5,19 @@ b = Blockchain.new
 
 
 get '/' do
- b.current_chain.to_s
+	message = ""
+	b.current_chain.each do |c|
+		message << "번호는" + c['index'].to_s + "입니다.<br>"
+		message << "Nonce" + c['nonce'].to_s + "입니다.<br>"
+		message << "시간은" + c['time'].to_s + "입니다.<br>"
+		message << "이전 주소는" + c['previous_block'].to_s + "입니다.<br>"
+		message << "내 주소는" + Digest::SHA256.hexdigest(c.to_s) + "입니다.<br>"
+		message << "<hr>"	
 end
+
+message
+end
+
 
 get '/mine'do
  b.mining
